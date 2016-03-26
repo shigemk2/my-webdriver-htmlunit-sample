fb6ffb3145704a1bdbc36971ed8877a123ecac2c
shigemk2 <i.am.shige@gmail.com>
2016-03-26 21:18:36 +0900

    classが複数設定されているときは全部指定しないとfindできない

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -11,9 +11,10 @@ object Hello {
     driver.get(url)
     println(s"title: ${driver.getTitle()}")
     driver.findElementByXPath("//div[@id='container']")
-      .findElements(By.xpath("//h1[@class='entry-title']"))
+      .findElements(By.xpath("//a[@class='entry-title-link bookmark']"))
       .foreach{ we: WebElement =>
         println(s"title: ${we.getText()}")
+        println(s"link: ${we.getAttribute("href")}")
       }
     println("Hello, world!")
   }
