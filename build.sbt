982925baef061bcc0fa3e143cb9d3dd9590dd106
shigemk2 <i.am.shige@gmail.com>
2016-03-26 21:13:32 +0900

    Add webdriver-htmlunit http://qiita.com/rkaneko/items/96588cf31b1ce3d41c79

--- build.sbt
+++ build.sbt
@@ -5,7 +5,10 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 // Change this to another test framework if you prefer
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+libraryDependencies ++= Seq(
+  "org.seleniumhq.webdriver" % "webdriver-htmlunit" % "0.9.7376",
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+)
 
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,20 @@
 package com.example
 
+import org.openqa.selenium.{By, WebElement}
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import scala.collection.JavaConversions._
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    val url = "http://www.shigemk2.com"
+    val driver = new HtmlUnitDriver()
+    driver.get(url)
+    println(s"title: ${driver.getTitle()}")
+    driver.findElementByXPath("//div[@id='container']")
+      .findElements(By.xpath("//h1[@class='entry-title']"))
+      .foreach{ we: WebElement =>
+        println(s"title: ${we.getText()}")
+      }
     println("Hello, world!")
   }
 }
